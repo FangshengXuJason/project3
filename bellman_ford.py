c87b60c21bafa8ff848b670ea592191661cc2037
FangshengXuaJason <xufangsheng@seattleu.edu>
2021-10-21 11:40:09 -0700

    added bellman_ford.py

---
+++ bellman_ford.py
@@ -0,0 +1,68 @@
+def bellman_ford(graph, source):
+    # Step 1: Prepare the distance and predecessor for each node
+    distance, predecessor = dict(), dict()
+    for node in graph:
+        distance[node], predecessor[node] = float('inf'), None
+    distance[source] = 0
+
+    # Step 2: Relax the edges
+    for _ in range(len(graph) - 1):
+        for node in graph:
+            for neighbour in graph[node]:
+                # If the distance between the node and the neighbour is lower than the current, store it
+                if distance[neighbour] > distance[node] + graph[node][neighbour]:
+                    distance[neighbour], predecessor[neighbour] = distance[node] + graph[node][neighbour], node
+
+    # Step 3: Check for negative weight cycles
+    for node in graph:
+        for neighbour in graph[node]:
+            # node -> src, start tracing
+            if distance[neighbour] > distance[node] + graph[node][neighbour]:
+                print("Negative weight cycle")
+                # print("child node\t-->\t parent node")
+                actions = []
+                child = node  # hard coded
+                while child in predecessor.keys():
+                    actions.append(child)
+                    child = predecessor[child]
+                    # print("{0}\t-->\t\t{1}".format(child, predecessor[child]))
+                    if child is node:  # hard coded
+                        actions.append(child)
+                        actions.reverse()
+                        print("actions", actions)
+                        break
+                    break
+    return distance, predecessor
+
+
+if __name__ == '__main__':
+    graph = {
+        'a': {'b': -1, 'c': 4},
+        'b': {'c': 3, 'd': 2, 'e': 2},
+        'c': {},
+        'd': {'b': 1, 'c': 5},
+        'e': {'d': -3}
+    }
+
+    distance, predecessor = bellman_ford(graph, source='a')
+    print(distance)
+
+    graph = {
+        'a': {'c': 3},
+        'b': {'a': 2},
+        'c': {'b': 7, 'd': 1},
+        'd': {'a': 6},
+    }
+
+    distance, predecessor = bellman_ford(graph, source='a')
+    print(distance)
+
+    graph = {
+        'a': {'b': 1},
+        'b': {'c': 2},
+        'c': {'a': -4},
+        # 'd': {'a': 6},
+    }
+
+    distance, predecessor = bellman_ford(graph, source='a')
+    print(distance)
--- test2.py
+++ test2.py
@@ -19,19 +19,27 @@ class Graph2:
     # utility function used to print the solution
     def printArr(self, dist):
         print("Vertex Distance from Source")
-        for i in range(self.V):
-            print("{0}\t\t{1}".format(i, dist[i]))
+        i = 0
+        for item in dist:
+            print("{0}\t\t{1}".format(i, item))
+            i = i + 1
 
-    def returnPath(self, end, start):
+    def printParent(self,):
+        print("Print Parent")
+        for child in self.parent.keys():
+            print("{0}\t-->\t\t{1}".format(child, self.parent[child]))
+
+    def returnPath(self, end):
         actions = []
         v = end
-        while (v in self.parent.keys()) and (v is not start):
-            parent = self.parent[v]
+        while v in self.parent.keys():
             actions.append(v)
-            v = self.parent[parent]
-        actions.reverse()
+            v = self.parent[v]
+            if v is end:
+                actions.append(v)
+                actions.reverse()
+                return actions
         return actions
-
     # src: start node/ edge
     def BellmanFord(self, src):
 
@@ -44,30 +52,48 @@ class Graph2:
                     distance[v] = distance[u] + w
                     self.parent[v] = u
         self.printArr(distance)
+        self.printParent()
+
         for u, v, w in self.graph:
             if distance[u] != float("Inf") and distance[u] + w < distance[v]:
                 print("Graph contains negative weight cycle")
-                print(self.returnPath(v, src))
+                print(self.returnPath(u))
                 return
 
 
 
 
-g = Graph2(5)
+# g = Graph2(5)
 
-g.addEdge(0, 1, -1)
-g.addEdge(2, 0, -3)
-g.addEdge(1, 2, 3)
+# g.addEdge(0, 1, -1)
+# g.addEdge(2, 0, -3)
+# g.addEdge(1, 2, 3)
+#
+# g.addEdge(2, 0, -3) # added
+#
+# g.addEdge(1, 3, 2)
+# g.addEdge(1, 4, 2)
+# g.addEdge(3, 2, 5)
+# g.addEdge(3, 1, 1)
+# g.addEdge(4, 3, -3)
 
-g.addEdge(2, 0, -3) # added
+g = Graph2(4)
 
-g.addEdge(1, 3, 2)
-g.addEdge(1, 4, 2)
-g.addEdge(3, 2, 5)
-g.addEdge(3, 1, 1)
-g.addEdge(4, 3, -3)
+g.addEdge(0,1,1)
+# g.addEdge(1,0,-1)
+
+g.addEdge(1,2,2)
+# g.addEdge(2,1,-2)
+
+g.addEdge(2,0,-4)
+# g.addEdge(0,2,4)
+
+g.addEdge(0,3,1)
+# g.addEdge(3,0,-1)
+
+g.addEdge(3,2,-1)
+# g.addEdge(2,3,1)
+
 # Print the solution
 g.BellmanFord(0)
 
-# Initially, Contributed by Neelam Yadav
-# Later On, Edited by Himanshu Garg
