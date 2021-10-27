8166638924b8af03361f23bb2c1ea1a3a271589c
Fangsheng Xu <fangcyxu@uw.edu>
2021-10-26 17:22:23 -0700

    created a new lab3.py(old one: lab3_old.py)

---
+++ lab3.py
@@ -0,0 +1,8 @@
+from fxp_bytes_subscriber import Subscriber
+
+if __name__ == '__main__':
+    print("testing lab3 subscriber")
+    lab3 = Subscriber()
+    lab3.subscribe()
+    lab3.read()
+    exit(1)
